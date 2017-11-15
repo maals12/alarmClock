e3bba7cb35554a9e1181fbebed434395b4c07141
Unknown <maarg@umich.edu>
2017-11-15 18:46:59 -0500

    Created alarmClock logic class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,6 +9,8 @@ public class Main {
 
     public static void main(String[] arg) {
 	    // write your code here
+        alarmClock.setTime();
+
         try {
             // Initializer
             URL_handler.getURLfromFile();
---
+++ src/com/company/alarmClock.java
@@ -0,0 +1,16 @@
+package com.company;
+
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class alarmClock {
+    public static void setTime(){
+        Date date = new Date();
+        long time = date.getTime();
+        System.out.println(date.getTime());
+    }
+
+    private static 
+}
